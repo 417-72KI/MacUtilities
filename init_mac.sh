bace632ff72dc12ecaf140421b18a4dfc0975af2
417-72KI <417.72ki@gmail.com>
2018-02-02 18:51:14 +0900

    Add Initialize script for new Mac

---
+++ init_mac.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# .bash_profileを作成
+if [ ! -f ~/.bash_profile ]; then
+    cat << EOS > ~/.bash_profile
+    if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+    fi
+EOS
+fi
+
+# エイリアスを登録する
+cat << EOS > ~/.bashrc
+alias ls='ls -G'
+alias ll='ls -la'
+EOS
+
+# 隠しファイルを表示するようにする
+defaults write com.apple.finder AppleShowAllFiles TRUE
+killall Finder
+
+# Homebrewインストール
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Node.jsインストール
+brew install nodebrew
+mkdir -p ~/.nodebrew/src
+nodebrew install-binary latest
+echo 'export PATH=$PATH:~/.nodebrew/current/bin' >> ~/.bashrc
